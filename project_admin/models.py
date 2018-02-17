1efe21e63a3bf7bb9c0d138bf2c509add738647a
fiennyangeln <fifi.angelina@yahoo.com>
2018-02-18 02:13:25 +0800

    add model project

--- project_admin/models.py
+++ project_admin/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+	id_ = models.IntegerField(primary_key=True)
+	basename = models.TextField()
+	created = models.DateField()
+	download_url = models.TextField()
+	complete = models.BooleanField(default=False)
+	dataYear = models.IntegerField()
+	description = models.TextField()
+	metadata = models.TextField() # in json
